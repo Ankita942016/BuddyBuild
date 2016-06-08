ab4af74f7840b14e97db887af66d82e32dc82e65
Soni, Ankita (US - Mumbai) <anksoni@deloitte.com>
2016-06-08 16:36:56 +0530

    [ci skip]

--- BuddyBuild/ViewController.swift
+++ BuddyBuild/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello")
+        print("Hello 2")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
